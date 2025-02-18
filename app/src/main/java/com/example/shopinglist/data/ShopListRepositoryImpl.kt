0ffd87c8ca2dfec0142fd7d5d2700704474910c9
skygrimm <skygrimm.super@gmail.com>
2025-02-18 20:18:37 +0200

    Recycler View
    1) Added recycler view pool
    2) Refactor onCreateViewHolder function to opportunity create 2 view types

--- app/src/main/java/com/example/shopinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/example/shopinglist/data/ShopListRepositoryImpl.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.shopinglist.domain.ShopItem
 import com.example.shopinglist.domain.ShopListRepository
+import kotlin.random.Random
 
 object ShopListRepositoryImpl: ShopListRepository {
     private val shopListLiveData = MutableLiveData<List<ShopItem>>()
@@ -13,7 +14,7 @@ object ShopListRepositoryImpl: ShopListRepository {
 
     init {
         for (i in 0 until 1000){
-            val item = ShopItem("Name $i", i, true)
+            val item = ShopItem("Name $i", i, Random.nextBoolean())
             addShopItem(item)
         }
     }
