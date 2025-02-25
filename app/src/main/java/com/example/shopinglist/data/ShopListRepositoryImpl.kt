b63a397791932358ff4ce26e348a7310045e7969
skygrimm <skygrimm.super@gmail.com>
2025-02-25 14:23:43 +0200

    Listeners
    1) LongClickListener was setup
    2) ClickListener was setup
    3) SwipeListener was setup
    
    Added comparator to sort rv list after change state

--- app/src/main/java/com/example/shopinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/example/shopinglist/data/ShopListRepositoryImpl.kt
@@ -8,12 +8,14 @@ import kotlin.random.Random
 
 object ShopListRepositoryImpl: ShopListRepository {
     private val shopListLiveData = MutableLiveData<List<ShopItem>>()
+    private val shopList = sortedSetOf<ShopItem>(
+        {o1, o2 -> o1.id.compareTo(o2.id)}
+    )
 
-    private val shopList = mutableListOf<ShopItem>()
     private var autoIncrementId = 0
 
     init {
-        for (i in 0 until 1000){
+        for (i in 0 until 10){
             val item = ShopItem("Name $i", i, Random.nextBoolean())
             addShopItem(item)
         }
--- app/src/main/java/com/example/shopinglist/presentation/ShopListAdapter.kt
+++ app/src/main/java/com/example/shopinglist/presentation/ShopListAdapter.kt
@@ -18,6 +18,9 @@ class ShopListAdapter : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>
             notifyDataSetChanged()
         }
 
+    var onShopItemLongClickListener:((ShopItem) -> Unit)? = null
+    var onShopItemClickListener:((ShopItem) -> Unit)? = null
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
         val layout = when (viewType) {
             VIEW_TYPE_ENABLED -> R.layout.item_shop_enabled
@@ -38,9 +41,16 @@ class ShopListAdapter : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>
         val shopItem = shopList[position]
         holder.tvName.text = shopItem.name
         holder.tvCount.text = shopItem.count.toString()
+
+
         holder.itemView.setOnLongClickListener {
+            onShopItemLongClickListener?.invoke(shopItem)
             true
         }
+        holder.itemView.setOnClickListener {
+            onShopItemClickListener?.invoke(shopItem)
+        }
+
     }
 
     override fun getItemViewType(position: Int): Int {
@@ -61,13 +71,16 @@ class ShopListAdapter : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>
         val tvCount: TextView = view.findViewById(R.id.tvCount)
     }
 
+    interface OnShopItemLongClickListener{
+        fun onShopItemLongClick(shopItem: ShopItem)
+    }
+
 
     companion object {
         const val VIEW_TYPE_ENABLED = 100
         const val VIEW_TYPE_DISABLED = 101
         const val MAX_POOL_SIZE = 5
     }
-
 
 }
 
